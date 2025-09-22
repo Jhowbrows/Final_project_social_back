df503ddd9788ad88a0bebed096b99a649c6a3388
Jhonatan <jhowbrows321@gmail.com>
2025-09-22 18:32:37 -0300

    fix: fixing allowed origins

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=pz^wp+-qwrbo0xasgsq9hxrd&l+hbssrn%pv=i)6&6rkqqpp)'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dummy-key-for-ci-tests')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,5 +128,5 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    'https://socialhub-henna.vercel.app/',
+    'https://socialhub-henna.vercel.app',
 ]
